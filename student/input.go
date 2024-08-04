292a73a8e1cc7b5161798f0a25c887c72fd9de1e
davodhiambo <jesseomolo@yahoo.com>
2024-08-04 20:18:03 +0300

    feat: add atoi function to utils.go

--- student/input.go
+++ student/input.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"bufio"
+	"os"
+)
+
+func scanInput() []int {
+	var input string
+	var data []int
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		input = scanner.Text()
+	}
+}
--- student/main.go
+++ student/main.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"os"
-	"bufio"
-)
-
 func main() {
-	var input string
-	var data []int
-	scanner := bufio.NewScanner(os.Stdin)
 
-	for scanner.Scan() {
-		input = scanner.Text()
-	}
 }
